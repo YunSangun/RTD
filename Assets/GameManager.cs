73f70fcd331d3c304d67b4d3c254a4763f98f679
coolguywon <tkaltk123@gmail.com>
2019-07-18 00:30:29 +0900

    gamemanager backup

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ELEMENT_TYPE
+{
+    FIRE,
+    ICE,
+    POISON,
+    IRON,
+    WIND
+}
+enum TILE_TYPE
+{
+
+}
+public class GameBoard
+{
+
+}
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
